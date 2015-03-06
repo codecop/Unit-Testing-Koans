1d1184f0e2e44aebb5fd6afdfc74fd223ec1242e
Peter Kofler <peter.kofler@code-cop.org>
2015-03-06 23:50:04 +0100

    session 1, working basic assertions

---
+++ php-phpunit/src/Greeter.php
@@ -0,0 +1,16 @@
+<?php
+
+class Greeter {
+
+    const GREETING = "Hello";
+
+    /**
+     * Return a proper greeting for a person.
+     */
+    public function greet($name) {
+        if (empty($name)) {
+            return self::GREETING;
+        }
+        return self::GREETING . " " . trim($name);
+    }
+}
---
+++ php-phpunit/test/Session1_GreeterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'Greeter.php';
+
+/**
+ * Session 1: GreeterTest - First Test methods against Greeter.
+ */
+class Session1_GreeterTest extends \PHPUnit_Framework_TestCase {
+
+    /** @test */
+    function shouldReturnHelloName() {
+        $greeter = new Greeter();
+        $this->assertEquals("Hello Peter", $greeter->greet("Peter"));
+    }
+
+    // add another test method, what about null?
+
+    /** @test */
+    function shouldReturnHelloForNull() {
+        $greeter = new Greeter();
+        $this->assertEquals("Hello", $greeter->greet(null));
+    }
+
+    // add another test method, what about blanks?
+
+    /** @test */
+    function shouldIgnoreWhitespace() {
+        $greeter = new Greeter();
+        $this->assertEquals("Hello Peter", $greeter->greet(" Peter "));
+    }
+
+}
