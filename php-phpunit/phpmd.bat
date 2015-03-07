c8a79bf757614e74b5614b7a748b90ebb1ab4b7e
Peter Kofler <peter.kofler@code-cop.org>
2015-03-07 01:06:04 +0100

    add PHPMD to project

---
+++ php-phpunit/phpmd.bat
@@ -0,0 +1,2 @@
+@call vendor\bin\phpmd.bat src text cleancode,codesize,controversial,design,naming,unusedcode
+@call vendor\bin\phpmd.bat test text cleancode,codesize,controversial,design,naming,unusedcode
