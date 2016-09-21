031f19b29c8e985d0ee43f223f227cdb4f0bf241
Goerge Albrecht <goerge@code-mentor.com>
2016-09-21 13:54:07 +0200

    replaces @Test and Assert -> Assertions

--- java-junit5/src/test/java/org/codecop/Session5_WordCounterRatioTest.java
+++ java-junit5/src/test/java/org/codecop/Session5_WordCounterRatioTest.java
@@ -1,14 +1,14 @@
 package org.codecop;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Session 5: WordCounterTableTest - parameterised/table driven. <br />
@@ -17,33 +17,32 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class Session5_WordCounterRatioTest {
 
-    // we want to test more corner cases for the ratio, here is a table of test cases
-    private static final List<Object[]> TEST_TABLE = Arrays.asList(//
-            new Object[] { "green", "green", 1.0 }, //
-            new Object[] { "green bar green", "green", 0.66 }, //
-            new Object[] { "green bar green bar", "green", 0.5 }, //
-            new Object[] { "green bar green", "bar", 0.33 } //
-            );
+  // we want to test more corner cases for the ratio, here is a table of test cases
+  private static final List<Object[]> TEST_TABLE = Arrays.asList(//
+    new Object[]{"green", "green", 1.0}, //
+    new Object[]{"green bar green", "green", 0.66}, //
+    new Object[]{"green bar green bar", "green", 0.5}, //
+    new Object[]{"green bar green", "bar", 0.33} //
+  );
 
-    @Parameters(name = "ratio of '{1}' in words '{0}' should be {2}")
-    public static List<Object[]> tableData() {
-        return TEST_TABLE;
-    }
+  @Parameters(name = "ratio of '{1}' in words '{0}' should be {2}")
+  public static List<Object[]> tableData() {
+    return TEST_TABLE;
+  }
 
-    private String sentence;
-    private String word;
-    private double expectedRratio;
+  private final String sentence;
+  private final String word;
+  private final double expectedRratio;
 
-    public Session5_WordCounterRatioTest(String sentence, String word, double expectedRratio) {
-        this.sentence = sentence;
-        this.word = word;
-        this.expectedRratio = expectedRratio;
-    }
+  public Session5_WordCounterRatioTest(final String sentence, final String word, final double expectedRratio) {
+    this.sentence = sentence;
+    this.word = word;
+    this.expectedRratio = expectedRratio;
+  }
 
-    @Test
-    public void shouldReturnRatioOfGivenWord() {
-        WordCounter counter = new WordCounter(sentence);
-        assertEquals(expectedRratio, counter.ratioOf(word), 0.01);
-    }
-
+  @Test
+  public void shouldReturnRatioOfGivenWord() {
+    final WordCounter counter = new WordCounter(sentence);
+    assertEquals(expectedRratio, counter.ratioOf(word), 0.01);
+  }
 }
--- java-junit5/src/test/java/org/codecop/StringToFileTest.java
+++ java-junit5/src/test/java/org/codecop/StringToFileTest.java
@@ -1,26 +1,26 @@
 package org.codecop;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.io.IOException;
 
 import org.junit.After;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringToFileTest {
 
-    private final File testFile = new File("StringToFileTest.tmp");
+  private final File testFile = new File("StringToFileTest.tmp");
 
-    @After
-    public void deleteTestFile() {
-        testFile.delete();
-    }
+  @After
+  public void deleteTestFile() {
+    testFile.delete();
+  }
 
-    @Test
-    public void shouldWriteAndRead() throws IOException {
-        String original = "Keep the bar green to keep the code clean.";
-        StringToFile.write(original, testFile);
-        assertEquals(original, StringToFile.read(testFile));
-    }
+  @Test
+  public void shouldWriteAndRead() throws IOException {
+    final String original = "Keep the bar green to keep the code clean.";
+    StringToFile.write(original, testFile);
+    assertEquals(original, StringToFile.read(testFile));
+  }
 }
