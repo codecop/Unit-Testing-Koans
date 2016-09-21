cfad65fbe59eef612e0492c6a06adcafd72ff008
Peter Kofler <peter.kofler@code-cop.org>
2016-09-21 21:23:58 +0200

    Removes final modifier and reorders import to reflect initial code samples.

--- java-junit5/src/test/java/org/codecop/Session1_GreeterTest.java
+++ java-junit5/src/test/java/org/codecop/Session1_GreeterTest.java
@@ -1,9 +1,9 @@
 package org.codecop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Session 1: GreeterTest - Your first tests.
@@ -12,20 +12,20 @@ class Session1_GreeterTest {
 
     @Test
     void shouldReturnHelloName() {
-        final Greeter greeter = new Greeter();
+        Greeter greeter = new Greeter();
         assertEquals("Hello Peter", greeter.greet("Peter"));
     }
 
     @Test
     @DisplayName("should return 'Hello' for 'null'")
     void shouldReturnHelloForNull() {
-        final Greeter greeter = new Greeter();
+        Greeter greeter = new Greeter();
         assertEquals("Hello", greeter.greet(null));
     }
 
     @Test
     void shouldIgnoreWhitespace() {
-        final Greeter greeter = new Greeter();
+        Greeter greeter = new Greeter();
         assertEquals("Hello Peter", greeter.greet(" Peter "));
     }
 }
