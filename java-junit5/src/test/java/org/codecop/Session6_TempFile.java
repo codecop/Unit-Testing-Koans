0850ad21c02c266bd1505d9dbdc331bfdc2e79b8
Peter Kofler <peter.kofler@code-cop.org>
2017-01-14 18:48:55 +0100

    Reorder and comment to make it easier implement the extension.

--- java-junit5/src/test/java/org/codecop/Session6_TempFile.java
+++ java-junit5/src/test/java/org/codecop/Session6_TempFile.java
@@ -18,7 +18,11 @@ import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.TestExtensionContext;
 
-class Session6_TempFile implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
+/**
+ * Session 6: TempFile - Temporary file Extensions. <br />
+ * See http://junit.org/junit5/docs/current/user-guide/#extensions
+ */
+class Session6_TempFile implements BeforeEachCallback, ParameterResolver, AfterEachCallback {
 
     @Target(ElementType.PARAMETER)
     @Retention(RetentionPolicy.RUNTIME)
@@ -26,32 +30,57 @@ class Session6_TempFile implements BeforeEachCallback, AfterEachCallback, Parame
         //
     }
 
-    private File tempFile;
-
+    // TODO Call the actual logic to create and destroy the temporary file 
+    // from the empty methods of the extension interfaces implemented.
+    
     @Override
     public void beforeEach(TestExtensionContext context) throws IOException {
-        tempFile = File.createTempFile(context.getTestInstance().getClass().getSimpleName(), ".tmp");
-
-        StringToFile.write("Keep the bar green to keep the code clean.", tempFile);
-    }
-
-    @Override
-    public void afterEach(TestExtensionContext context) {
-        assertTrue(tempFile.delete()); // keep
+        Object testInstance = context.getTestInstance();
+        // TODO create a temporary file before each test
+        createTempFileFor(testInstance); // drop
     }
 
     @Override
     public boolean supports(ParameterContext parameterContext, ExtensionContext context) {
         Parameter parameter = parameterContext.getParameter();
-        return parameter.isAnnotationPresent(Temp.class) && parameter.getType() == File.class;
+        boolean parameterIsAnnotated = parameter.isAnnotationPresent(Temp.class);
+        boolean parameterExpectsFile = parameter.getType() == File.class;
+
+        return parameterIsAnnotated && parameterExpectsFile;
     }
 
     @Override
     public Object resolve(ParameterContext parameterContext, ExtensionContext context) throws ParameterResolutionException {
+        // TODO return the temporary file  whenever a test method parameter has been resolved to use it.
+        // use return null;
+        return getTempFile(); // drop
+    }
+
+    @Override
+    public void afterEach(TestExtensionContext context) {
+        // TODO remove the temporary file after each test
+        removeTempFile(); // drop
+    }
+    
+    // --- actual temporary file logic
+    
+    private File tempFile;
+
+    private void createTempFileFor(Object testInstance) throws IOException {
+        tempFile = File.createTempFile(testInstance.getClass().getSimpleName(), ".tmp");
+
+        StringToFile.write("Keep the bar green to keep the code clean.", tempFile);
+    }
+
+    private File getTempFile() {
         if (!tempFile.exists() || tempFile.length() == 0) {
             throw new ParameterResolutionException("Could not create temp file " + tempFile);
         }
         return tempFile;
     }
-
+    
+    private void removeTempFile() {
+        assertTrue(tempFile.delete()); // keep
+    }
+    
 }
