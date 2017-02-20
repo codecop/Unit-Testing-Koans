396eaa073516a76ef103232515c70ce8e56ebf1a
Peter Kofler <peter.kofler@code-cop.org>
2017-02-20 23:45:34 +0100

    Comment assertions (with script), remove script.

--- java-junit5/src/test/java/org/codecop/Session6_WordCounterFileExtensionTest.java
+++ java-junit5/src/test/java/org/codecop/Session6_WordCounterFileExtensionTest.java
@@ -14,18 +14,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * Session 6: WordCounterFileExtensionTest - Reuse fixtures in Extensions. <br />
  * See http://junit.org/junit5/docs/current/user-guide/#extensions
  */
-@ExtendWith(Session6_TempFile.class) // Use the TempFile extension to create a temporary words file for each test. 
+// TODO // Use the TempFile extension to create a temporary words file for each test. 
 class Session6_WordCounterFileExtensionTest {
 
     @Test
     void shouldReturnCountOfWordsBetter(@Temp File wordsFile) throws IOException {
         WordCounter counter = new WordCounter(wordsFile);
-        assertEquals(9, counter.numberOfWords()); // keep
+        assertEquals(9, counter.numberOfWords()); 
     }
 
     @Test
     void shouldVerifyContainmentOfWord(@Temp File wordsFile) throws IOException {
         WordCounter counter = new WordCounter(wordsFile);
-        assertTrue(counter.containsWord("bar")); // keep
+        assertTrue(counter.containsWord("bar")); 
     }
 }
