4b17398b93b4cfc2627d07eccc4e25627d1e48fb
Peter Kofler <peter.kofler@code-cop.org>
2018-02-25 22:45:34 +0100

    Factor comment assertions script.

--- java-junit5/comment_assertions.rb
+++ java-junit5/comment_assertions.rb
@@ -1,79 +1,104 @@
-Dir.chdir('src/test/java')
+#! ruby
 
-Dir['**/Session*.java'].each do |file_name|
-  # report
+def modify(file_name)
   puts "modifying #{file_name}"
-
-  # load
-  lines = IO.readlines(file_name)
-
-  # modify
-  lines = lines.map do |line|
-    if line =~ /\/\/ keep/
-      # leave that line alone, just remove the marker
-      "#{$`}#{$'}"
-
-    elsif line =~ /\/\/ use\s/
-      # remove comment to use code that was commented
-      "#{$`}#{$'}"
-
-    elsif line =~ /\/\/ drop\s*$/
-      # drop line
-      "#{$'}"
-
-    elsif line =~ /(\S.*) = assertThrows\((.*)\);/
-      "#{$`}// TODO Expect #{$1} is thrown from #{$2}.#{$'}"
-
-    elsif line =~ /assert(\w+)\((.*)\);/
-      # comment general assertions
-      front = "#{$`}// TODO Check that "
-      how = ''
-      back = ".#{$'}"
-
-      if $1 == "ArrayEquals" or $1 == "That" or $1 == "True"
-        what = $2
-      elsif $1 == "Equals"
-        what = $2.sub(/, 0\.01/, ''). # remove double rounding
-                  sub(/Integer.valueOf\(2\)/, '2') # remove boxing
-      elsif $1 == "Timeout"
-        what = "#{$2} has a "
-        how = $1.downcase()
-      else
-        what = "#{$2} is "
-        how = $1.downcase().
-          sub(/not/, "not "). # needs to be last to preserve $0
-          sub(/iterable/, "iterable "). 
-          sub(/equals/, "equal"). 
-          sub(/throws/, "thrown")
-      end
-
-      front + what + how + back
-
-    elsif line =~ /@TestFactory|@BeforeEach|@AfterEach|@ExtendWith\([^)]+\)/
-      # comment test factory, life cycle, extension
-      "#{$`}// TODO#{$'}"
-
-    elsif line =~ /@Disabled\((.*)\)/
-      # comment disabled
-      "#{$`}// TODO Mark this test as ignored with #{$1}.#{$'}"
-
-    else
-
-      # regular line
-      line
-    end
-  end
-
-  # save
-  File.open(file_name, "w") { |file|
-    lines.each { |line| file.print line }
-  }
+  lines = load(file_name)
+  lines = lines.map { |line| comment_assertion_in(line) }
+  save(file_name, lines)
 end
 
-# remove useless test
-['org/codecop/StringToFileTest.java'].each do |file_name|
+def load(file_name)
+  IO.readlines(file_name)
+end
+
+def comment_assertion_in(line)
+  if line =~ /\/\/ keep/
+    # leave that line alone, just remove the marker
+    "#{$`}#{$'}"
+
+  elsif line =~ /\/\/ use\s/
+    # remove comment to use code that was commented
+    "#{$`}#{$'}"
+
+  elsif line =~ /\/\/ drop\s*$/
+    # drop line
+    "#{$'}"
+
+  elsif line =~ /(\S.*) = assertThrows\((.*)\);/
+    "#{$`}// TODO Expect #{$1} is thrown from #{$2}.#{$'}"
+
+  elsif line =~ /assert(\w+)\((.*)\);/
+    # comment general assertions
+    comment_assert($`, $1, $2, $') 
+    
+  elsif line =~ /@TestFactory|@BeforeEach|@AfterEach|@ExtendWith\([^)]+\)/
+    # comment test factory, life cycle, extension
+    "#{$`}// TODO#{$'}"
+
+  elsif line =~ /@Disabled\((.*)\)/
+    # comment disabled
+    "#{$`}// TODO Mark this test as ignored with #{$1}.#{$'}"
+
+  else
+    # regular line
+    line
+    
+  end
+end
+
+def comment_assert(before, term, args, after)
+  front = "#{before}// TODO Check that "
+  how = ''
+  back = ".#{after}"
+
+  if term == "ArrayEquals" or term == "That" or term == "True"
+    what = args
+  
+  elsif term == "Equals"
+    what = args.sub(/, 0\.01/, ''). # remove double rounding
+              sub(/Integer.valueOf\(2\)/, '2') # remove boxing
+
+  elsif term == "Timeout"
+    what = "#{args} has a "
+    how = term.downcase()
+  
+  else
+    what = "#{args} is "
+    how = term.downcase().
+      sub(/not/, "not "). 
+      sub(/iterable/, "iterable "). 
+      sub(/equals/, "equal"). 
+      sub(/throws/, "thrown")
+  end
+
+  front + what + how + back
+end
+
+def save(file_name, lines)
+  File.open(file_name, "w") do |file|
+    lines.each { |line| file.print line }
+  end
+end
+
+def delete(file_name)
   puts "deleting #{file_name}"
   File.delete(file_name)
 end
 
-Dir.chdir('../../..')
+if __FILE__ == $0
+
+  Dir.chdir('src/test/java')
+  
+  # comment session files
+  Dir['**/Session*.java'].each do |file_name|
+    modify(file_name)
+  end
+  
+  # remove useless files
+  ['org/codecop/StringToFileTest.java'].each do |file_name|
+    delete(file_name)
+  end
+  
+  Dir.chdir('../../..')
+
+end
