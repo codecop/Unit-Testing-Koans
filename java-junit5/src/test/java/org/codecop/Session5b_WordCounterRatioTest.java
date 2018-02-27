f36805b3bed347c07e52d9f6ae0d0dac61677126
Peter Kofler <peter.kofler@code-cop.org>
2018-02-27 22:03:05 +0100

    Session 5b, parameterised tests by Daniel M?hlbachler.

---
+++ java-junit5/src/test/java/org/codecop/Session5b_WordCounterRatioTest.java
@@ -0,0 +1,44 @@
+package org.codecop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * Session 5b: WordCounterRatioTest - (experimental) Parameterised tests. <br />
+ * @see "https://junit.org/junit5/docs/current/user-guide/#writing-tests-parameterized-tests"
+ * 
+ * @author Daniel Mühlbachler
+ */
+class Session5b_WordCounterRatioTest {
+
+    // TODO Add the needed annotations to run this test with all examples.
+
+    @Test
+    void convinceIntelliJThatThisIsValidJUnit5Test() {
+        // convince IntelliJ that this is a valid JUnit5 test
+    }
+
+    // We want to test more corner cases for the ratio. Here is a table of test cases.
+    static Stream<Arguments> getTestCases() {
+        return Stream.of(//
+                Arguments.of("green", "green", 1.0), //
+                Arguments.of("green bar green", "green", 0.66), //
+                Arguments.of("green bar green bar", "green", 0.5), //
+                Arguments.of("green bar green", "bar", 0.33) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestCases")
+    void shouldReturnRatioOfGivenWord(String sentence, String word, double expectedRatio) {
+        WordCounter counter = new WordCounter(sentence);
+        assertEquals(expectedRatio, counter.ratioOf(word), 0.01); // keep
+    }
+
+}
