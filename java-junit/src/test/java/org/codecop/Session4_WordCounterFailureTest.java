c0e8ee581ed46e4913573e9fe36a741f32da2cc4
Peter Kofler <peter.kofler@code-cop.org>
2018-07-03 23:29:46 +0200

    Split catchException out of 4.

--- java-junit/src/test/java/org/codecop/Session4_WordCounterFailureTest.java
+++ java-junit/src/test/java/org/codecop/Session4_WordCounterFailureTest.java
@@ -1,23 +1,25 @@
 package org.codecop;
 
-import static com.googlecode.catchexception.CatchException.catchException;
-import static com.googlecode.catchexception.CatchException.caughtException;
-import static com.googlecode.catchexception.CatchException.verifyException;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * Session 4: WordCounterFailureTest - testing for Exceptions and ignoring tests. <br />
- * See https://github.com/junit-team/junit/wiki/Exception-testing
+ * Session 4: WordCounterFailureTest - Exceptions and ignoring tests. <br />
+ * @see "https://github.com/junit-team/junit/wiki/Exception-testing"
  */
 public class Session4_WordCounterFailureTest {
+
+    // TODO Add the needed code/annotations to test for an expected exception.
+
+    @Test(expected = IOException.class)
+    public void shouldThrowIOExceptionOnMissingFile() throws IOException {
+        new WordCounter(new File("IamSureThisDoesNotExist.txt"));
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowIllegalArgumentExceptionForUnknownWord() {
@@ -25,33 +27,14 @@ public class Session4_WordCounterFailureTest {
         counter.ratioOf("missingWord");
     }
 
-    /**
-     * optional - catchException can do it better. <br />
-     * See https://github.com/Codearte/catch-exception
-     */
-    @Test
-    public void shouldThrowInvalidArgumentExceptionAlternative() {
-        WordCounter counter = new WordCounter("green bar green");
-        verifyException(counter, IllegalArgumentException.class).ratioOf("anotherMissingWord");
-
-        // optional - Hamcrest can do it better
-        catchException(counter).ratioOf("anotherMissingWord");
-        assertThat(caughtException(), IsInstanceOf.instanceOf(IllegalArgumentException.class));
-    }
-
-    @Test(expected = IOException.class)
-    public void shouldThrowIOExceptionOnMissingFile() throws IOException {
-        new WordCounter(new File("IamSureThisDoesNotExist.txt"));
-    }
-
-    // the next test does not work, we need to change the code,
-    // but we will do that tomorrow. for today ignore it
+    // TODO The next test does not work, we need to change the code,
+    // but we will do that tomorrow. For today let's ignore it.
 
     @Test
     @Ignore("work in progress, will continue tomorrow")
     public void shouldCountUniqueWordsCaseInsensitive() {
         WordCounter counter = new WordCounter("green bar Green hat");
-        assertArrayEquals(new String[] { "bar", "green", "hat" }, counter.uniqueWords());
+        assertArrayEquals(new String[] { "bar", "green", "hat" }, counter.uniqueWords()); // keep
     }
 
 }
