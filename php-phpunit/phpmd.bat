091e38b6019f544dae4f8ffd540c81935d3c89f8
Peter Kofler <peter.kofler@code-cop.org>
2018-07-09 21:39:48 +0200

    Remove Codesniffer and PHPMD.

--- php-phpunit/phpmd.bat
+++
@@ -1,2 +0,0 @@
-@call vendor\bin\phpmd.bat src text cleancode,codesize,controversial,design,naming,unusedcode
-@call vendor\bin\phpmd.bat test text cleancode,codesize,controversial,design,naming,unusedcode
