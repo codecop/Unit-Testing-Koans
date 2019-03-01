a0f346b30f3f94830604ddfa89573bc2cdd78c15
Peter Kofler <peter.kofler@code-cop.org>
2019-03-01 18:08:55 +0100

    Complete session 4 exceptions.

--- csharp-xunit/WordCountTests/Session4_WordCounterFailureTest.cs
+++ csharp-xunit/WordCountTests/Session4_WordCounterFailureTest.cs
@@ -1,30 +1,36 @@
 using Org.Codecop.WordCount;
+using System;
 using System.IO;
 using Xunit;
 
 namespace Org.Codecop.WordCount.Tests
 {
     /// <summary>Session 4: WordCounterFailureTest - Exceptions and ignoring tests.</summary>
-    /// <seealso>"TODO"</seealso>
+    /// <seealso>"https://hadihariri.com/2008/10/17/testing-exceptions-with-xunit/"</seealso>
     public class Session4_WordCounterFailureTest
     {
         // TODO Add the needed code/annotations to test for an expected exception.
 
+        [Fact]
         public void ShouldThrowIOExceptionOnMissingFile()
         {
-            new WordCounter(new FileInfo("IamSureThisDoesNotExist.txt"));
+            Action wordCountOfMissingFile = () => new WordCounter(new FileInfo("IamSureThisDoesNotExist.txt"));
+            Assert.Throws(typeof(FileNotFoundException), wordCountOfMissingFile);
         }
 
-        public void ShouldThrowIllegalArgumentExceptionForUnknownWord()
+        [Fact]
+        public void ShouldThrowArgumentExceptionForUnknownWord()
         {
             var counter = new WordCounter("green bar green");
-            counter.RatioOf("missingWord");
+            Action ratioOfMissingWord = () => counter.RatioOf("missingWord");
+            var exception = Assert.Throws(typeof(ArgumentException), ratioOfMissingWord);
+            Assert.Equal("missingWord not in sentence", exception.Message);
         }
 
         // TODO The next test does not work, we need to change the code,
         // but we will do that tomorrow. For today let's ignore it.
 
-        [Fact]
+        [Fact(Skip = "work in progress, will continue tomorrow")]
         public void ShouldCountUniqueWordsCaseInsensitive()
         {
             var counter = new WordCounter("green bar Green hat");
