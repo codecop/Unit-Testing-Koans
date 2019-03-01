3a9367e0e9c1239c77fc48f8522bb229f714e5b1
Peter Kofler <peter.kofler@code-cop.org>
2019-03-01 18:16:48 +0100

    koan for InlineData.

--- csharp-xunit/WordCountTests/Session5_WordCounterRatioTest.cs
+++ csharp-xunit/WordCountTests/Session5_WordCounterRatioTest.cs
@@ -5,44 +5,18 @@ using Xunit;
 namespace Org.Codecop.WordCount.Tests
 {
     /// <summary>Session 5: WordCounterRatioTest - parameterised/table driven tests.</summary>
-    /// <seealso>"TODO"</seealso>
+    /// <seealso>"http://hamidmosalla.com/2017/02/25/xunit-theory-working-with-inlinedata-memberdata-classdata/"</seealso>
     public class Session5_WordCounterRatioTest
     {
-        private static readonly IList<object[]> TestCases = Arrays.AsList(
-            new object[] { "green", "green", 1.0 },
-            new object[] { "green bar green", "green", 0.66 },
-            new object[] { "green bar green bar", "green", 0.5 },
-            new object[] { "green bar green", "bar", 0.33 });
 
-        // Mark this test as parameterised
         // TODO Add the needed annotations to run this test with all examples.
         // We want to test more corner cases for the ratio. Here is a table of test cases.
-        //
-        //
-        //
-        //
-        //
-        public static IList<object[]> GetTestCases()
-        {
-            // This method returns the parameters
-            return TestCases;
-        }
-
-        private readonly string sentence;
-
-        private readonly string word;
-
-        private readonly double expectedRatio;
-
-        public Session5_WordCounterRatioTest(string sentence, string word, double expectedRratio)
-        {
-            this.sentence = sentence;
-            this.word = word;
-            this.expectedRatio = expectedRratio;
-        }
-
-        [Fact]
-        public void ShouldReturnRatioOfGivenWord()
+        [Theory]
+        [InlineData("green", "green", 1.0)]
+        [InlineData("green bar green", "green", 0.66)]
+        [InlineData("green bar green bar", "green", 0.5)]
+        [InlineData("green bar green", "bar", 0.33)]
+        public void ShouldReturnRatioOfGivenWord(string sentence, string word, double expectedRatio)
         {
             var counter = new WordCounter(sentence);
             Assert.Equal(expectedRatio, counter.RatioOf(word), 3); // keep
